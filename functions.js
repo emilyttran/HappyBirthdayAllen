6dd2a1a4523a4a50f0001d9b5c64801c4d74e0a9
Emily Tran <trane@oxy.edu>
2017-11-07 00:20:23 -0800

    finished finished

--- functions.js
+++ functions.js
@@ -1,66 +1,173 @@
-// Based on 'JPEG Raster' by Jonathan Puckey:
+var pic = new Raster('allen.jpg');
+var count = 4;
 
-// Create a raster item:
-var raster = new Raster('allen.jpg',view.center);
-var loaded = false;
+var allen= new Symbol(pic);
 
-raster.on('load', function() {
-	loaded = true;
-	onResize();
-});
-
-// Make the raster invisible:
-raster.visible = false;
-
-var lastPos = view.center;
-function moveHandler(event) {
-	if (!loaded)
-		return;
-	if (lastPos.getDistance(event.point) < 10)
-		return;
-	lastPos = event.point;
-
-	var size = this.bounds.size.clone();
-	var isLandscape = size.width > size.height;
-
-	// If the path is in landscape orientation, we're going to
-	// split the path horizontally, otherwise vertically:
-
-	size /= isLandscape ? [2, 1] : [1, 2];
-
-	var path = new Path.Rectangle({
-		point: this.bounds.topLeft.floor(),
-		size: size.ceil(),
-		onMouseMove: moveHandler
-	});
-	path.fillColor = raster.getAverageColor(path);
-
-	var path = new Path.Rectangle({
-		point: isLandscape
-			? this.bounds.topCenter.ceil()
-			: this.bounds.leftCenter.ceil(),
-		size: size.floor(),
-		onMouseMove: moveHandler
-	});
-	path.fillColor = raster.getAverageColor(path);
-
-	this.remove();
+// Place the instances of the symbol:
+for (var i = 0; i < count; i++) {
+	// The center position is a random point in the view:
+  var temp = count*3;
+	var center = Point.random() * view.size;
+	var placedSymbol1 = allen.place(center);
+	placedSymbol1.scale(i / temp);
 }
 
-function onResize(event) {
-	if (!loaded)
-		return;
-	project.activeLayer.removeChildren();
+var pic2 = new Raster('koala.jpg');
+var count = 4;
 
-	// Transform the raster so that it fills the bounding rectangle
-	// of the view:
-	raster.fitBounds(view.bounds, true);
+var koala = new Symbol(pic2);
 
-	// Create a path that fills the view, and fill it with
-	// the average color of the raster:
-	new Path.Rectangle({
-		rectangle: view.bounds,
-		fillColor: raster.getAverageColor(view.bounds),
-		onMouseMove: moveHandler
+// Place the instances of the symbol:
+for (var i = 0; i < count; i++) {
+	// The center position is a random point in the view:
+  var temp = count*3;
+	var center = Point.random() * view.size;
+	var placedSymbol1 = koala.place(center);
+	placedSymbol1.scale(i / temp);
+}
+
+function Ball(r, p, v) {
+	this.radius = r;
+	this.point = p;
+	this.vector = v;
+	this.maxVec = 15;
+	this.numSegment = Math.floor(r / 3 + 2);
+	this.boundOffset = [];
+	this.boundOffsetBuff = [];
+	this.sidePoints = [];
+	this.path = new Path({
+		fillColor: {
+			hue: Math.random() * 360,
+			saturation: 1,
+			brightness: 1
+		},
+		blendMode: 'lighter'
 	});
+
+	for (var i = 0; i < this.numSegment; i ++) {
+		this.boundOffset.push(this.radius);
+		this.boundOffsetBuff.push(this.radius);
+		this.path.add(new Point());
+		this.sidePoints.push(new Point({
+			angle: 360 / this.numSegment * i,
+			length: 1
+		}));
+	}
+}
+
+Ball.prototype = {
+	iterate: function() {
+		this.checkBorders();
+		if (this.vector.length > this.maxVec)
+			this.vector.length = this.maxVec;
+		this.point += this.vector;
+		this.updateShape();
+	},
+
+	checkBorders: function() {
+		var size = view.size;
+		if (this.point.x < -this.radius)
+			this.point.x = size.width + this.radius;
+		if (this.point.x > size.width + this.radius)
+			this.point.x = -this.radius;
+		if (this.point.y < -this.radius)
+			this.point.y = size.height + this.radius;
+		if (this.point.y > size.height + this.radius)
+			this.point.y = -this.radius;
+	},
+
+	updateShape: function() {
+		var segments = this.path.segments;
+		for (var i = 0; i < this.numSegment; i ++)
+			segments[i].point = this.getSidePoint(i);
+
+		this.path.smooth();
+		for (var i = 0; i < this.numSegment; i ++) {
+			if (this.boundOffset[i] < this.radius / 4)
+				this.boundOffset[i] = this.radius / 4;
+			var next = (i + 1) % this.numSegment;
+			var prev = (i > 0) ? i - 1 : this.numSegment - 1;
+			var offset = this.boundOffset[i];
+			offset += (this.radius - offset) / 15;
+			offset += ((this.boundOffset[next] + this.boundOffset[prev]) / 2 - offset) / 3;
+			this.boundOffsetBuff[i] = this.boundOffset[i] = offset;
+		}
+	},
+
+	react: function(b) {
+		var dist = this.point.getDistance(b.point);
+		if (dist < this.radius + b.radius && dist != 0) {
+			var overlap = this.radius + b.radius - dist;
+			var direc = (this.point - b.point).normalize(overlap * 0.015);
+			this.vector += direc;
+			b.vector -= direc;
+
+			this.calcBounds(b);
+			b.calcBounds(this);
+			this.updateBounds();
+			b.updateBounds();
+		}
+	},
+
+	getBoundOffset: function(b) {
+		var diff = this.point - b;
+		var angle = (diff.angle + 180) % 360;
+		return this.boundOffset[Math.floor(angle / 360 * this.boundOffset.length)];
+	},
+
+	calcBounds: function(b) {
+		for (var i = 0; i < this.numSegment; i ++) {
+			var tp = this.getSidePoint(i);
+			var bLen = b.getBoundOffset(tp);
+			var td = tp.getDistance(b.point);
+			if (td < bLen) {
+				this.boundOffsetBuff[i] -= (bLen  - td) / 2;
+			}
+		}
+	},
+
+	getSidePoint: function(index) {
+		return this.point + this.sidePoints[index] * this.boundOffset[index];
+	},
+
+	updateBounds: function() {
+		for (var i = 0; i < this.numSegment; i ++)
+			this.boundOffset[i] = this.boundOffsetBuff[i];
+	}
+};
+
+var balls = [];
+var numBalls = 11;
+for (var i = 0; i < numBalls; i++) {
+	var position = Point.random() * view.size;
+	var vector = new Point({
+		angle: 360 * Math.random(),
+		length: Math.random() * 10
+	});
+	var radius = Math.random() * 60 + 60;
+	balls.push(new Ball(radius, position, vector));
+}
+
+function onFrame(event) {
+  for (var i = 0; i < count+4; i++) {
+  		var item = project.activeLayer.children[i];
+
+  		// Move the item 1/20th of its width to the right. This way
+  		// larger circles move faster than smaller circles:
+  		item.position.x += item.bounds.width / 70;
+
+  		// If the item has left the view on the right, move it back
+  		// to the left:
+  		if (item.bounds.left > view.size.width) {
+  			item.position.x = -item.bounds.width;
+  		}
+  	}
+	for (var i = 0; i < balls.length - 1; i++) {
+		for (var j = i + 1; j < balls.length; j++) {
+			balls[i].react(balls[j]);
+		}
+	}
+	for (var i = 0, l = balls.length; i < l; i++) {
+		balls[i].iterate();
+	}
 }
